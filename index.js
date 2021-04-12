4db5a89074b7987137470e1d976bd651caeaee5c
Damet <damet.2401@gmail.com>
2021-04-12 17:43:47 -0500

    first commit

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-PORT = 8080;
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, console.log(`server on port ${PORT}`));
